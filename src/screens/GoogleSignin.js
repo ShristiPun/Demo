19cd7d3ea5b373e99ec8943c2cae00bdc9a4909b
ShristiPun <55776369+ShristiPun@users.noreply.github.com>
2020-12-02 18:37:33 +0545

    demo

---
+++ src/screens/GoogleSignin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+//import GoogleSignin, { GoogleSigninButton } from '@react-native-community/google-signin'
+
+
+const GoogleSignin = () => {
+    return (
+        <View>
+            <Text>ggggg</Text>
+        </View>
+    )
+}
+
+
+
+
+export default GoogleSignin
